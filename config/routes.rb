6a1c9d8ea191685a4bfd6472ede12614ac72e460
Julio Quintana <julioeq29@gmail.com>
2021-03-12 18:40:20 +0000

    photo style for desktop

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#the_batch'
   # Pages
   # get :the_batch, to: 'pages#the_batch'
-  get :the_numbers,  to: 'pages#the_numbers'
+  get :the_students,      to: 'pages#the_students'
   get :the_projects, to: 'pages#the_projects'
-  get :the_TAs,      to: 'pages#the_TAs'
+  get :the_numbers,  to: 'pages#the_numbers'
   get :quick_note,   to: 'pages#quick_note'
 end
